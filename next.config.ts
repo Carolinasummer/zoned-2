b248a7c42bbe6c5f53e58188d12fabb1312d83af
Carolinasummer <9r5y7ptzz5@privaterelay.appleid.com>
2026-07-30 01:41:09 +0300

    правки5

--- next.config.ts
+++ next.config.ts
@@ -19,3 +19,18 @@ export default config;
 };
 
 export default config;
+import type { NextConfig } from "next";
+
+const config: NextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default config;
